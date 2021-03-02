3ee304698347859ea0ad8a6f7239becd99bc5405
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-02 17:59:11 -0300

    UserModel created

---
+++ src/model/UserModel.ts
@@ -0,0 +1,40 @@
+// Services
+import { Verify } from "../business/services/verify"
+
+
+// Entities
+import { UserDTO, UserInput } from "../business/entities/UserInterfaces"
+
+
+export class UserModel {
+
+    // Transform UserInput + generated id into UserDTO.
+    static inputToUserDTO = (user: UserInput, id: string): UserDTO => {
+        const { name, nickname, email, password, avatar } = user
+        return {
+            id,
+            name,
+            nickname,
+            email,
+            password,
+            avatar
+        }
+    }
+
+
+    // Attempt to create an UserInput, throws error if not possible
+    static anyToUserInput = (user: any, verify: Verify): UserInput => {
+        const validKeys = ["name", "nickname", "email", "password", "avatar"]
+        verify.objectKeys(user, validKeys)
+
+        const { name, nickname, email, password, avatar } = user
+
+        return {
+            name,
+            nickname,
+            email,
+            password,
+            avatar
+        }
+    }
+}
