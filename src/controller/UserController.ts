a2f09019e7f0d3b20603a228b262624c8014fe1c
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-02 18:00:27 -0300

    UserController with signup method created

---
+++ src/controller/UserController.ts
@@ -0,0 +1,36 @@
+// External libraries
+import { Request, Response } from "express"
+
+// Business layer classes and services
+import { UserBusiness } from "../business/UserBusiness"
+
+
+
+// Model and Entities
+import { UserInput } from "../business/entities/UserInterfaces"
+import { UserModel } from "../model/UserModel"
+import { Verify } from "../business/services/verify"
+
+
+export class UserController {
+    constructor(
+        private userBusiness: UserBusiness
+    ) { }
+
+
+    public signup = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const input: UserInput = UserModel.anyToUserInput(req.body, new Verify())
+
+            const accessData = await this.userBusiness.createUser(input)
+
+            res.status(201).send({
+                message: "User created successfully.",
+                accessData
+            })
+
+        } catch (error) {
+            res.status(error.code || 400).send(error.message || error.sqlMessage)
+        }
+    }
+}
