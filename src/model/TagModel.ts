84cf8e87524fcb786a5c008c6c7cddf42a85d878
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-04 12:39:56 -0300

    Created a TagModel with tag to TagDTO method

---
+++ src/model/TagModel.ts
@@ -0,0 +1,26 @@
+// Services
+import { Verify } from "../business/services/verify"
+
+
+
+
+// Entities
+import { ImageInput } from "../business/entities/couplingInterfaces"
+import { TagDTO } from "../business/entities/tagInterfaces"
+
+
+
+export class TagModel {
+
+    // Transforms a tag + generated id into TagDTO
+    static readonly tagToTagDTO = (tag: string, id: string, verifier: Verify): TagDTO => {
+        verifier.string(tag)
+        verifier.hashtag(tag)
+
+        return {
+            id,
+            tag
+        }
+    }
+    
+}
