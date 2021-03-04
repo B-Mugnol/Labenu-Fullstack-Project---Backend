d9ab83b82c951022b172a8a52167c8b8fdfda6ba
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-04 12:37:49 -0300

    Added verifications

--- src/model/ImageModel.ts
+++ src/model/ImageModel.ts
@@ -1,5 +1,5 @@
 // Services
-
+import { Verify } from "../business/services/verify"
 
 
 // Entities
@@ -10,18 +10,24 @@ import { ImageDTO } from "../business/entities/imageInterfaces"
 export class ImageModel {
 
     // Transform ImageInput + generated id into ImageDTO
-    static readonly inputToImageDTO = (input: ImageInput, id: string): ImageDTO => {
-        const { subtitle, creation_date, file_path, file, collection, author_id } = input
+    static readonly inputToImageDTO = (input: ImageInput, id: string, authorId: string, verifier: Verify): ImageDTO => {
+        const validKeys = ["subtitle", "file_path", "file", "collection"]
+        const optionalKeys = ["file_path", "file"]
+        verifier.objectKeys(input, validKeys, optionalKeys)
+
+        const { subtitle, file_path, file, collection } = input
+
+        verifier.string({ subtitle, file_path, collection })
 
         return {
             id,
             subtitle,
-            creation_date,
+            creation_date: new Date().getTime(),
             file_path,
             file,
             collection,
-            author_id
+            author_id: authorId
         }
     }
-    
+
 }
