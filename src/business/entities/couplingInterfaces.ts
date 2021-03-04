4ea816f4a038f02773b2d819a5e0c22134dac0c4
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-04 12:38:37 -0300

    Changed ImageInput to reflect the real input

--- src/business/entities/couplingInterfaces.ts
+++ src/business/entities/couplingInterfaces.ts
@@ -7,10 +7,8 @@ export interface allImageInfoDTO extends ImageDTO {
 
 export interface ImageInput {
     subtitle: string,
-    creation_date: number,
     file_path: string | null,
     file: Blob | null,
     collection: string,
-    author_id: string,
     tags: string[]
 }
