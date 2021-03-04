f350ac8a37a715bc2f19cf6c71ca3e96ef47defc
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-04 10:44:21 -0300

    Added getUNtaggedImagesByUserId method

--- src/data/ImageDatabase.ts
+++ src/data/ImageDatabase.ts
@@ -35,4 +35,31 @@ export class ImageDatabase extends BaseDatabase {
             throw new Error(error.sqlMessage || error.message)
         }
     }
+
+    public getUntaggedImagesByUserId = async (
+        userId: string,
+        perPage?: number,
+        pageNumber?: number
+    ): Promise<ImageDTO> => {
+        try {
+            const result: any = await this.connection.raw(`
+                SELECT * FROM ${this.tableNames.images}
+                WHERE
+                    author_id = "${userId}"
+                ${perPage ?
+                    "LIMIT " + perPage + "\n" +
+                        "OFFSET " + pageNumber ? 5 * (pageNumber! - 1) : 1
+                    :
+                    "LIMIT 1000"
+
+                }
+                ;
+            `)
+
+            return result[0]
+
+        } catch (error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
 }
