c54835aeb063644ced04530917b8dafff57c1587
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-05 11:08:12 -0300

    Added login method to UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -22,15 +22,37 @@ export class UserController {
         try {
             const validKeys = ["name", "nickname", "email", "password", "avatar"]
             this.verifier.objectKeys(req.body, validKeys)
-            
-            this.verifier.string(req.body) // Verifies if all fields are of the correct type: string
-            
+
+            this.verifier.string(req.body)
+
             const input: UserInput = UserModel.anyToUserInput(req.body)
 
             const accessData = await this.userBusiness.createUser(input)
 
             res.status(201).send({
                 message: "User created successfully.",
+                accessData
+            })
+
+        } catch (error) {
+            res.status(error.code || 400).send(error.message || error.sqlMessage)
+        }
+    }
+
+
+    public readonly login = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const validKeys = ["email", "password"]
+            this.verifier.objectKeys(req.body, validKeys)
+
+            this.verifier.string(req.body)
+
+            const { email, password } = req.body
+
+            const accessData = await this.userBusiness.login(email, password)
+
+            res.status(200).send({
+                message: "User logged in successfully.",
                 accessData
             })
 
