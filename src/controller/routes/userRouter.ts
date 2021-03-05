d690619c83bf31040028a38f3c321f3e48c9ec6a
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-05 09:39:10 -0300

    Organized imports

--- src/controller/routes/userRouter.ts
+++ src/controller/routes/userRouter.ts
@@ -1,17 +1,25 @@
 // External library
 import express from "express"
-import { HashManager } from "../../business/services/hashManager"
-import { IdManager } from "../../business/services/idManager"
-import { TokenManager } from "../../business/services/tokenManager"
-import { Verify } from "../../business/services/verify"
+
+// User Business, Database and Controller
 import { UserBusiness } from "../../business/UserBusiness"
 import { UserDatabase } from "../../data/UserDatabase"
 import { UserController } from "../UserController"
 
+// Services
+import { HashManager } from "../../business/services/hashManager"
+import { IdManager } from "../../business/services/idManager"
+import { TokenManager } from "../../business/services/tokenManager"
+import { Verify } from "../../business/services/verify"
+
 export const userRouter = express.Router()
 
 const userBusiness = new UserBusiness(
-    new UserDatabase, new IdManager, new HashManager, new TokenManager, new Verify
+    new UserDatabase,
+    new IdManager,
+    new HashManager,
+    new TokenManager,
+    new Verify
 )
 
 userRouter.post("/signup", new UserController(userBusiness).signup)
