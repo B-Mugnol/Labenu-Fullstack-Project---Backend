11d8cf3304451d3e3c2d97d139f5bb595761d48b
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-05 10:07:26 -0300

    Fixed need for a Verify parameter in creating a UserController instance

--- src/controller/routes/userRouter.ts
+++ src/controller/routes/userRouter.ts
@@ -1,16 +1,19 @@
 // External library
 import express from "express"
 
+
 // User Business, Database and Controller
 import { UserBusiness } from "../../business/UserBusiness"
 import { UserDatabase } from "../../data/UserDatabase"
 import { UserController } from "../UserController"
+
 
 // Services
 import { HashManager } from "../../business/services/hashManager"
 import { IdManager } from "../../business/services/idManager"
 import { TokenManager } from "../../business/services/tokenManager"
 import { Verify } from "../../business/services/verify"
+
 
 export const userRouter = express.Router()
 
@@ -22,4 +25,8 @@ const userBusiness = new UserBusiness(
     new Verify
 )
 
-userRouter.post("/signup", new UserController(userBusiness).signup)
+userRouter.post(
+    "/signup",
+    new UserController(userBusiness, new Verify)
+        .signup
+)
