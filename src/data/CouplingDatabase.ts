6d7c856dfaf328d18ec28c4e0457c44a37aaf8ed
B-Mugnol <bruno_mugnol@hotmail.com>
2021-03-11 09:47:27 -0300

    Reducing DB responsibility, passing to Business

--- src/data/CouplingDatabase.ts
+++ src/data/CouplingDatabase.ts
@@ -2,64 +2,26 @@
 import { BaseDatabase } from "./BaseDatabase"
 
 
-// Databases
-import { UserDatabase } from "./UserDatabase"
-import { ImageDatabase } from "./ImageDatabase"
-import { ImageTagRelationDatabase } from "./ImageTagRelationDatabase"
-import { TagDatabase } from "./TagDatabase"
-
-
 // Entities
-import { allImageInfoDTO } from "../business/entities/couplingInterfaces";
-import { ImageDTO } from "../business/entities/imageInterfaces"
-
-
-// Errors
-import { NotFoundError } from "../error/NotFoundError"
+import { TagDTO } from "../business/entities/tagInterfaces"
 
 
 export class CouplingDatabase extends BaseDatabase {
 
-    constructor(
-        private userDatabase: UserDatabase,
-        private imageDatabase: ImageDatabase,
-        private relationDatabase: ImageTagRelationDatabase,
-        private tagDatabase: TagDatabase
-    ) {
-        super()
-    }
-
-
-    public getImagesByUserId = async (
-        userId: string,
-        perPage?: number,
-        pageNumber?: number): Promise<allImageInfoDTO[]> => {
+    public getTagsFromImageId = async (
+        imageId: string
+    ): Promise<TagDTO[]> => {
         try {
-            const basicImage: ImageDTO[] | undefined = await this.imageDatabase.getUntaggedImagesByUserId(
-                userId,
-                perPage,
-                pageNumber
-            )
-
-            if (!basicImage) throw new NotFoundError("User has created no images.")
-
-            const imageArray: allImageInfoDTO[] = []
-
-            basicImage.forEach(async image => {
-                const tagsObjArray: { tag: string }[] = await this.connection.raw(`
+            const result: any = await this.connection.raw(`
                     SELECT tag FROM ${this.tableNames.images} img
                     JOIN ${this.tableNames.imageTagRelation} r
-                        ON r.image_id = ${image.id}
+                        ON r.image_id = img.id
                     JOIN ${this.tableNames.tags} t
                         ON t.id = r.tag_id
+                    WHERE img.id = "${imageId}"
                 `)
 
-                const tags: string[] = tagsObjArray.map(tagObj => tagObj.tag)
-
-                imageArray.push({ ...image, tags })
-            });
-
-            return imageArray
+            return result[0]
 
         } catch (error) {
             throw new Error(error.sqlMessage || error.message)
